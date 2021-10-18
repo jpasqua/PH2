598bb70a8e9712404de0c9c09258376b6ae1363c
Joe Pasqua <joe@noroomattheinn.org>
2021-10-18 15:32:59 -0700

    Adjust labels if there is no weather sensor

--- src/screens/HomeScreen.cpp
+++ src/screens/HomeScreen.cpp
@@ -2,6 +2,7 @@
 //                                  Core Libraries
 #include <Arduino.h>
 //                                  Third Party Libraries
+#include <Output.h>
 #include <TimeLib.h>
 //                                  WebThingApp
 #include <gui/Display.h>
@@ -15,9 +16,13 @@ static inline uint16_t compose(int h, int m) { return(h * 100 + m); }
 
 static constexpr auto Reading_Font = Display.FontID::S10;
 static constexpr uint16_t Reading_FontHeight = 13;
-static constexpr uint16_t Reading_XOrigin = 1;
+static constexpr uint16_t Reading_XOrigin = 0;
 static constexpr uint16_t Reading_YOrigin = 51;
-static constexpr uint16_t Reading_Width = 63;
+#if defined(HAS_WEATHER_SENSOR)
+  static constexpr uint16_t Reading_Width = 63;
+#else
+  static constexpr uint16_t Reading_Width = 128;
+#endif
 static constexpr uint16_t Reading_Height = Reading_FontHeight;
 static constexpr uint16_t Reading_BorderSize = 1;
 
@@ -26,12 +31,7 @@ static constexpr uint16_t Label_FontHeight = 13;
 
 
 HomeScreen::HomeScreen() {
-  // uint16_t x = Reading_XOrigin;
-  // labels = new Label[NCurrencyLabels];
-  // for (int i = 0; i < NCurrencyLabels; i++) {
-  //   labels[i].init(x, Reading_YOrigin, Reading_Width, Reading_Height, i);
-  //   x += Reading_Width - Reading_BorderSize;  // Make the borders overlap
-  // }
+
 }
 
 void HomeScreen::display(bool) {
@@ -75,16 +75,23 @@ void HomeScreen::drawLabels() {
   Display.oled->setColor(Theme::Color_NormalText);
   Display.setFont(Label_Font);
 
-  uint16_t yPos = Reading_YOrigin - Label_FontHeight + 1; // Add an extra pixel for padding
-  Display.oled->drawString(Display.XCenter/2, yPos, "AQI");
-  Display.oled->drawString(Display.XCenter + Display.XCenter/2, yPos, "Temp");
+    uint16_t yPos = Reading_YOrigin - Label_FontHeight + 1; // Add an extra pixel for padding
+    Display.oled->drawString(Reading_Width/2, yPos, "AQI");
+  #if defined(HAS_WEATHER_SENSOR)
+    Display.oled->drawString(Reading_Width + Reading_Width/2, yPos, "Temp");
+  #endif
 }
 
 void HomeScreen::drawReadings() {
-  Label l;
+  const AQIReadings& aqiReadings = phApp->aqiMgr.getLastReadings();
+  String aqiToDisplay(phApp->aqiMgr.derivedAQI(aqiReadings.env.pm25));
 
+  Label l;
   l.init(0, Reading_YOrigin, Reading_Width, Reading_Height, 0);
-  l.drawSimple("34", Reading_Font, 1, WHITE, WHITE, BLACK);
-  l.region.x += Display.Width/2;
-  l.drawSimple("74", Reading_Font, 1, WHITE, WHITE, BLACK);
+  l.drawSimple(aqiToDisplay, Reading_Font, Reading_BorderSize, WHITE, WHITE, BLACK);
+#if defined(HAS_WEATHER_SENSOR)
+  int temp = Output::temp(phApp->weatherMgr.getLastReadings().temp);
+  l.region.x += Reading_Width;
+  l.drawSimple(String(temp), Reading_Font, Reading_BorderSize, WHITE, WHITE, BLACK);
+#endif
 }
