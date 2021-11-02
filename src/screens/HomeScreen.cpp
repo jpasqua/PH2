709b979d0f34584d35349a62501f8c09da93b6cd
Joe Pasqua <joe@noroomattheinn.org>
2021-11-02 11:45:52 -0700

    Multiple improvements
    * Display data depending on which sensors are available
    * Fix bug with time display
    * Add more data to bottom readout

--- src/screens/HomeScreen.cpp
+++ src/screens/HomeScreen.cpp
@@ -18,11 +18,7 @@ static constexpr auto Reading_Font = Display.FontID::S10;
 static constexpr uint16_t Reading_FontHeight = 13;
 static constexpr uint16_t Reading_XOrigin = 0;
 static constexpr uint16_t Reading_YOrigin = 51;
-#if defined(HAS_WEATHER_SENSOR)
-  static constexpr uint16_t Reading_Width = 63;
-#else
-  static constexpr uint16_t Reading_Width = 128;
-#endif
+static constexpr uint16_t Reading_Width = 43;
 static constexpr uint16_t Reading_Height = Reading_FontHeight;
 static constexpr uint16_t Reading_BorderSize = 1;
 
@@ -39,24 +35,31 @@ void HomeScreen::display(bool) {
   
   oled->clear();
 
-  bool am = true;
-  int  m = minute();
-  int  h = hour();
+  bool use24Hour = phSettings->uiOptions.use24Hour;
+  time_t curTime = now();
+  int  m = minute(curTime);
+  int  h = hour(curTime);
   compositeTime = compose(h, m);
 
-  if (h > 12) { h -= 12; am = false; }
-  else if (h == 0) { h = 12;}
-  else if (h == 12) { am = false; }
+  if (!use24Hour) {
+    if (h > 12) { h -= 12; }
+    else if (h == 0) { h = 12;}
+  }
 
   constexpr uint8_t bufSize = 32;
   char buf[bufSize];
   snprintf(buf, bufSize, "%d:%02d", h, m);
   Display.setFont(Display.FontID::D32);
-  oled->setTextAlignment(TEXT_ALIGN_RIGHT);
-  oled->drawString(Display.Width-24, 2, buf);
-  oled->setTextAlignment(TEXT_ALIGN_LEFT);
-  Display.setFont(Display.FontID::S10);
-  oled->drawString(Display.Width-20, 4, am ? "AM" : "PM");
+  if (use24Hour) {
+    oled->setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
+    oled->drawString(Display.XCenter, Display.getFontHeight(Display.FontID::D32)/2 + 2, buf);
+  } else {
+    oled->setTextAlignment(TEXT_ALIGN_RIGHT);
+    oled->drawString(Display.Width-24, 2, buf);
+    oled->setTextAlignment(TEXT_ALIGN_LEFT);
+    Display.setFont(Display.FontID::S10);
+    oled->drawString(Display.Width-20, 4, isAM(curTime) ? "AM" : "PM");
+  }
 
   drawLabels();
   drawReadings();
@@ -75,23 +78,51 @@ void HomeScreen::drawLabels() {
   Display.oled->setColor(Theme::Color_NormalText);
   Display.setFont(Label_Font);
 
-    uint16_t yPos = Reading_YOrigin - Label_FontHeight + 1; // Add an extra pixel for padding
-    Display.oled->drawString(Reading_Width/2, yPos, "AQI");
-  #if defined(HAS_WEATHER_SENSOR)
-    Display.oled->drawString(Reading_Width + Reading_Width/2, yPos, "Temp");
+  uint16_t xPos = 1;
+  uint16_t yPos = Reading_YOrigin - Label_FontHeight + 1; // Add an extra pixel for padding
+  #if defined(HAS_AQI_SENSOR)
+    // AQI    TEMP        HUMI
+    Display.oled->drawString(xPos + Reading_Width/2, yPos, "aqi");
+    xPos += Reading_Width;
+    Display.oled->drawString(xPos + Reading_Width/2, yPos, "temp");
+    xPos += Reading_Width;
+    Display.oled->drawString(xPos + Reading_Width/2, yPos, "humi");
+  #else
+    // TEMP    HUMI       BARO
+    Display.oled->drawString(xPos + Reading_Width/2, yPos, "temp");
+    xPos += Reading_Width;
+    Display.oled->drawString(xPos + Reading_Width/2, yPos, "humi");
+    xPos += Reading_Width;
+    Display.oled->drawString(xPos + Reading_Width/2, yPos, "baro");
   #endif
 }
 
 void HomeScreen::drawReadings() {
-  const AQIReadings& aqiReadings = phApp->aqiMgr.getLastReadings();
-  String aqiToDisplay(phApp->aqiMgr.derivedAQI(aqiReadings.env.pm25));
+
+  int readings[3];
+  #if defined(HAS_AQI_SENSOR) && defined(HAS_WEATHER_SENSOR)
+    // AQI    TEMP        HUMI
+    readings[0] = phApp->aqiMgr.derivedAQI(phApp->aqiMgr.getLastReadings().env.pm25);
+    readings[1] = phApp->weatherMgr.getLastReadings().temp;
+    readings[2] = phApp->weatherMgr.getLastReadings().humidity;
+  #elif defined(HAS_AQI_SENSOR)
+    // AQI    OWM_TEMP    OWM_HUMI
+    readings[0] = phApp->aqiMgr.derivedAQI(phApp->aqiMgr.getLastReadings().env.pm25);
+    readings[1] = phApp->owmClient->weather.readings.temp;
+    readings[2] = phApp->owmClient->weather.readings.humidity;
+  #else
+    // TEMP    HUMI       BARO
+    auto wReadings = phApp->weatherMgr.getLastReadings();
+    readings[0] = wReadings.temp;
+    readings[1] = wReadings.humidity;
+    readings[2] = wReadings.pressure;
+  #endif
+
 
   Label l;
-  l.init(0, Reading_YOrigin, Reading_Width, Reading_Height, 0);
-  l.drawSimple(aqiToDisplay, Reading_Font, Reading_BorderSize, WHITE, WHITE, BLACK);
-#if defined(HAS_WEATHER_SENSOR)
-  int temp = Output::temp(phApp->weatherMgr.getLastReadings().temp);
-  l.region.x += Reading_Width;
-  l.drawSimple(String(temp), Reading_Font, Reading_BorderSize, WHITE, WHITE, BLACK);
-#endif
+  l.init(1, Reading_YOrigin, Reading_Width, Reading_Height, 0);
+  for (int i = 0; i < 3; i++) {
+    l.drawSimple(String(readings[i]), Reading_Font, Reading_BorderSize, WHITE, WHITE, BLACK);
+    l.region.x += Reading_Width-1;
+  }
 }
